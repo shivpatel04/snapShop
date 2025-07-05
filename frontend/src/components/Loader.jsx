58a861ac5f238d2d8c7d0590872210403d8c97bf
Abhishek0-7 <118382627+Abhishek0-7@users.noreply.github.com>
2025-07-05 21:27:56 +0530

    Update Loader.jsx

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => (
+  <div className="loader-container">
+    <div className="spinner"></div>
+    <p>Processing your image...</p>
+  </div>
+);
+
+export default Loader;
