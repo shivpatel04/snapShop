b52354aaee83b41098bb5de61a94eed48931e4fc
Abhishek0-7 <118382627+Abhishek0-7@users.noreply.github.com>
2025-07-05 21:31:08 +0530

    Update App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UploadForm from "./components/UploadForm";
+import { Container } from "@mui/material";
+
+function App() {
+  return (
+    <Container maxWidth="md">
+      <UploadForm />
+    </Container>
+  );
+}
+
+export default App;
+
